b4320d2ecd1e83e31d865e98f23a110d25851016
Genc, Alper Numan <alper.numan.genc@accenture.com>
2017-10-23 18:53:23 +0300

    URI convert improved by using algorithm stackoverflow discussion: https://stackoverflow.com/questions/17546101/get-real-path-for-uri-android

--- android/src/main/java/com/anumang/rnfileutils/RNFileUtilsModule.java
+++ android/src/main/java/com/anumang/rnfileutils/RNFileUtilsModule.java
@@ -6,6 +6,7 @@ import android.os.Environment;
 import android.provider.DocumentsContract;
 import android.provider.MediaStore;
 import android.database.Cursor;
+import android.content.ContentUris;
 
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
@@ -30,6 +31,11 @@ public class RNFileUtilsModule extends ReactContextBaseJavaModule {
 
   private  static final String CONTENT_SCHEME = "content";
   private  static final String FILE_SCHEME = "file";
+
+  private  static final String PRIMARY_TYPE = "primary";
+  private  static final String IMAGE_TYPE = "image";
+  private  static final String VIDEO_TYPE = "video";
+  private  static final String AUDIO_TYPE = "audio";
 
   private static final String RNFUDocumentDirectoryPath = "RNFUDocumentDirectoryPath";
   private static final String RNFUPicturesDirectoryPath = "RNFUPicturesDirectoryPath";
@@ -64,27 +70,27 @@ public class RNFileUtilsModule extends ReactContextBaseJavaModule {
       }
 
       private String getDocumentDirectory() {
-        return Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS).getPath();
+        return Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS).getAbsolutePath();
       }
 
       private String getDownloadsDirectory() {
-        return Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getPath();
+        return Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getAbsolutePath();
       }
 
       private String getPicturesDirectory() {
-        return Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).getPath();
+        return Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).getAbsolutePath();
       }
 
       private String getDCIMDirectory() {
-        return Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM).getPath();
+        return Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM).getAbsolutePath();
       }
 
       private String getCachesDirectory() {
-        return _reactContext.getCacheDir().getPath();
+        return _reactContext.getCacheDir().getAbsolutePath();
       }
 
       private String getFilesDirectory() {
-        return _reactContext.getFilesDir().getPath();
+        return _reactContext.getFilesDir().getAbsolutePath();
       }
 
     });
@@ -100,47 +106,92 @@ public class RNFileUtilsModule extends ReactContextBaseJavaModule {
    */
   @ReactMethod
   public void getPathFromURI(String uriString, Promise promise) {
-    Uri uri = Uri.parse(uriString);
-    String resultPath = null;
-    try {
-      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT && DocumentsContract.isDocumentUri(_reactContext, uri)) {
-        final String[] id = getDocumentUriId(uri);
-        resultPath = extractDataColumn(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, ID_SELECTOR, id);
-      } else if (CONTENT_SCHEME.equalsIgnoreCase(uri.getScheme())){
-        resultPath = extractDataColumn(uri, null, null);
-      } else if (FILE_SCHEME.equalsIgnoreCase(uri.getScheme())) {
-        resultPath = uri.getPath();
+  try{
+      Uri uri = Uri.parse(uriString);
+      final boolean isKitKat = Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT;
+
+      // DocumentProvider
+      if (isKitKat && DocumentsContract.isDocumentUri(_reactContext, uri)) {
+        // ExternalStorageProvider
+        if (isExternalStorageDocument(uri)) {
+          final String docId = DocumentsContract.getDocumentId(uri);
+          final String[] split = docId.split(":");
+          final String type = split[0];
+
+          if (PRIMARY_TYPE.equalsIgnoreCase(type)) {
+            promise.resolve(Environment.getExternalStorageDirectory() + "/" + split[1]);
+          }
+
+          // TODO handle non-primary volumes
+        }
+        // DownloadsProvider
+        else if (isDownloadsDocument(uri)) {
+
+          final String id = DocumentsContract.getDocumentId(uri);
+          final Uri contentUri = ContentUris.withAppendedId(
+                  Uri.parse("content://downloads/public_downloads"), Long.valueOf(id));
+
+          promise.resolve(getDataColumn(contentUri, null, null));
+        }
+        // MediaProvider
+        else if (isMediaDocument(uri)) {
+          final String docId = DocumentsContract.getDocumentId(uri);
+          final String[] split = docId.split(":");
+          final String type = split[0];
+
+          Uri contentUri = null;
+          if (IMAGE_TYPE.equals(type)) {
+            contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+          } else if (VIDEO_TYPE.equals(type)) {
+            contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+          } else if (AUDIO_TYPE.equals(type)) {
+            contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+          }
+
+          final String selection = ID_SELECTOR;
+          final String[] selectionArgs = new String[] {
+                  split[1]
+          };
+
+          promise.resolve(getDataColumn(contentUri, selection, selectionArgs));
+        }
       }
-      if(resultPath == null) {
-        promise.reject(ERROR_UNHANDLED_FILE_TYPE, "File location could not detected.");
-      } else {
-        promise.resolve(resultPath);
+      // MediaStore (and general)
+      else if (CONTENT_SCHEME.equalsIgnoreCase(uri.getScheme())) {
+
+        // Return the remote address
+        if (isGooglePhotosUri(uri))
+          promise.resolve(uri.getLastPathSegment());
+
+        promise.resolve(getDataColumn( uri, null, null));
       }
-    } catch (Exception ex) {
-      ex.printStackTrace();
-      promise.reject(ERROR_UNEXPECTED_EXCEPTION, ex.getMessage());
-    }
+      // File
+      else if (FILE_SCHEME.equalsIgnoreCase(uri.getScheme())) {
+        promise.resolve(uri.getPath());
+      }
+
+      promise.reject(ERROR_UNHANDLED_FILE_TYPE, "File location could not detected.");
+  } catch (Exception ex) {
+    ex.printStackTrace();
+    promise.reject(ERROR_UNEXPECTED_EXCEPTION, ex.getMessage());
+  }
   }
 
-  /**
-   * Get the value of the data column for this Uri. This is useful for
-   * MediaStore Uris, and other file-based ContentProviders.
-   *
-   * @param uri The Uri to query.
-   * @param selection (Optional) Filter used in the query.
-   * @param selectionArgs (Optional) Selection arguments used in the query.
-   * @return The value of the _data column, which is typically a file path.
-   */
-  public static String extractDataColumn(Uri uri, String selection,
+  public static String getDataColumn(Uri uri, String selection,
                                      String[] selectionArgs) {
 
     Cursor cursor = null;
+    final String column = DATA_COLUMN;
+    final String[] projection = {
+            column
+    };
+
     try {
-      cursor = _reactContext.getContentResolver().query(uri, new String[]{DATA_COLUMN}, selection, selectionArgs,
+      cursor = _reactContext.getContentResolver().query(uri, projection, selection, selectionArgs,
               null);
       if (cursor != null && cursor.moveToFirst()) {
-        final int column_index = cursor.getColumnIndexOrThrow(DATA_COLUMN);
-        return cursor.getString(column_index);
+        final int index = cursor.getColumnIndexOrThrow(column);
+        return cursor.getString(index);
       }
     } finally {
       if (cursor != null)
@@ -149,8 +200,36 @@ public class RNFileUtilsModule extends ReactContextBaseJavaModule {
     return null;
   }
 
-  private String[] getDocumentUriId(Uri uri) {
-    String idString =  DocumentsContract.getDocumentId(uri);
-    return new String[] {idString.split(":")[1]};
+
+  /**
+   * @param uri The Uri to check.
+   * @return Whether the Uri authority is ExternalStorageProvider.
+   */
+  public static boolean isExternalStorageDocument(Uri uri) {
+    return "com.android.externalstorage.documents".equals(uri.getAuthority());
+  }
+
+  /**
+   * @param uri The Uri to check.
+   * @return Whether the Uri authority is DownloadsProvider.
+   */
+  public static boolean isDownloadsDocument(Uri uri) {
+    return "com.android.providers.downloads.documents".equals(uri.getAuthority());
+  }
+
+  /**
+   * @param uri The Uri to check.
+   * @return Whether the Uri authority is MediaProvider.
+   */
+  public static boolean isMediaDocument(Uri uri) {
+    return "com.android.providers.media.documents".equals(uri.getAuthority());
+  }
+
+  /**
+   * @param uri The Uri to check.
+   * @return Whether the Uri authority is Google Photos.
+   */
+  public static boolean isGooglePhotosUri(Uri uri) {
+    return "com.google.android.apps.photos.content".equals(uri.getAuthority());
   }
 }
